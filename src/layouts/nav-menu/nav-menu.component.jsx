e867df28cd7083d78e10d63065e06957e09e7a17
VikViku <s.vikvik@gmail.com>
2023-10-30 20:56:03 +0200

    add dropdown menu

--- src/layouts/nav-menu/nav-menu.component.jsx
+++ src/layouts/nav-menu/nav-menu.component.jsx
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+
 
 import './nav-menu.style.scss';
 
-const NavMenu = () => (
-    <nav>
-        <input type="checkbox" id="nav-toggle"/>
-        <label htmlFor="nav-toggle" className="burger-menu">
-            <span className="burger-icon">&nbsp;</span>
-        </label>
-        <Link to="/" className="logo">FIORI STORI</Link>
-        <ul className="menu">
-            <li><Link to="/paslaugos">Paslaugos</Link></li>
-            <li><Link to="/nuoma">Nuoma</Link></li>
-            <li><Link to="/atsiliepimai">Atsiliepimai</Link></li>
-            <li><Link to="/apie-mane">Apie mane</Link></li>
-            <li><Link to="/galerija">Galerija</Link>
-                <div class="dropdown">
-                    <ul>
-                        <li><Link to="/">Vestuvės</Link></li>
-                        <li><Link to="/">Krikštynos ir gimtadieniai</Link></li>
-                        <li><Link to="/">Kitos šventės</Link></li>
-                    </ul>
-                </div>
-            </li>
-            <li><Link to="/kontaktai">Kontaktai</Link></li>
-        </ul>   
-    </nav>
+const NavMenu = () => {
+    const [isHovered, setIsHovered] = useState(false);
 
-    );
+    return (
+        <nav>
+            <input type="checkbox" id="nav-toggle"/>
+            <label htmlFor="nav-toggle" className="burger-menu">
+                <span className="burger-icon">&nbsp;</span>
+            </label>
+            <Link to="/" className="logo">FIORI STORI</Link>
+            <ul className="menu">
+                <li><Link to="/paslaugos">Paslaugos</Link></li>
+                <li><Link to="/nuoma">Nuoma</Link></li>
+                <li><Link to="/atsiliepimai">Atsiliepimai</Link></li>
+                <li><Link to="/apie-mane">Apie mane</Link></li>
+                <li onMouseEnter={() => setIsHovered(!isHovered)} 
+                    onMouseLeave={() => setIsHovered(!isHovered)} >
+                <Link>Galerija &nbsp; 
+                {isHovered ? (<FontAwesomeIcon icon={faCaretUp}/>) :
+                (<FontAwesomeIcon icon={faCaretDown}/>)}</Link>
+                    <div className="dropdown">
+                        <ul>
+                            <li><Link to="/Galerija">Vestuvės</Link></li>
+                            <li><Link to="/Galerija">Krikštynos ir gimtadieniai</Link></li>
+                            <li><Link to="/Galerija">Kitos šventės</Link></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><Link to="/kontaktai">Kontaktai</Link></li>
+            </ul>   
+        </nav>
+    )
+
+};
 
 export default NavMenu;
