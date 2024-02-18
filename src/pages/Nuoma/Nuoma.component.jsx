a87e48219f9eb6b432746a29cd00c1cbde3c8f6f
VikViku <s.vikvik@gmail.com>
2024-02-18 13:11:19 +0200

    make more responsible

--- src/pages/Nuoma/Nuoma.component.jsx
+++ src/pages/Nuoma/Nuoma.component.jsx
@@ -7,49 +7,49 @@ import Heading from '../../components/heading/heading.component';
 const Nuoma = () => (
     <div className="Nuoma">
     <Heading title="Nuoma"/>
-    <button class="action-button" role="button">Nuomos sąlygos</button>
+    <button class="action-button">Nuomos sąlygos</button>
 
     <div className="renting">
             <div>
                 <Link to="/Nuoma/arkos">
-                    <img src="https://placehold.co/300x200"/>
+                    <img src="https://placehold.co/300x200" alt=''/>
                 </Link>
                 <p>Arkos / foto sienos</p>
             </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Saldaus stalo indai</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Vazos</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Žvakidės</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Stalo numeriukai</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Arkos / foto sienos</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Saldaus stalo indai</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Vazos</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Žvakidės</p>
         </div>
         <div>
-            <img src="https://placehold.co/300x200"/>
+            <img src="https://placehold.co/300x200" alt=''/>
             <p>Stalo numeriukai</p>
         </div>    </div>
 
